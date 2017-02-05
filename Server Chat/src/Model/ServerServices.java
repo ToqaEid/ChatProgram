ccba69a8e4d6bbfaaee62e218d73d6b39adbaace
ToqaEid <toqa.eid@gmail.com>
2017-02-05 16:27:39 +0200

    interfaces added

--- Server Chat/src/Model/ServerServices.java
+++ Server Chat/src/Model/ServerServices.java
@@ -5,14 +5,21 @@
  */
 package Model;
 import DataTransferObject.*;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 /**
  *
  * @author Samir
  */
-public interface ServerServices {
-    void signUp(User newUser);
-    void signIn(String userEmail,String userPassword);
-    void addFriend(ContactList contactList);
-    void changeStatus(User user);
-    String getUserStatus(String userEmail);
+public interface ServerServices extends Remote{
+    boolean signUp(User newUser) throws RemoteException;
+    User signIn(User user) throws RemoteException;
+    ArrayList<ContactList> getContactList(User user) throws RemoteException;
+    ArrayList<UserMsg> getOfflineRequests(User user) throws RemoteException;
+    void addFriend(ContactList list) throws RemoteException;
+    void registerFriendInContactList(ContactList list) throws RemoteException;
+    void signOut(String userEmail) throws RemoteException;
+    void changeUserStatus(User user, String status, ArrayList<ContactList> contacts) throws RemoteException;
+    void tellClient(UserMsg msg) throws RemoteException;
 }
